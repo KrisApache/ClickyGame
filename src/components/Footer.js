364670d4f326f12c04fd4110604121f9816a01e5
Shrish Chaturvedi <syrus989@gmail.com>
2018-06-30 00:52:17 -0500

    working

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+    <div className="footer navbar-fixed-bottom navbar-light bg-danger" style={{ height: "100px" }}>
+        <p className="text-center" style={{ size: "20px", weight: "bolder" }}>Clicky Game</p>
+    </div>
+)
+
+export default Footer;
